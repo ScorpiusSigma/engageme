c75dbbda99f88528e8c979835bc4b8c1c6266ede
ScorpiusSigma <justinleng_@hotmail.com>
2023-05-14 15:37:41 +0800

    Add hashkey

--- src/pages/api/attendance.ts
+++ src/pages/api/attendance.ts
@@ -20,11 +20,22 @@ import {
 	Metaplex,
 } from "@metaplex-foundation/js";
 import base58 from "bs58";
+import { createHash } from "crypto";
 
-// Devnet 'fake' USDC, you can get these tokens from https://spl-token-faucet.com/
-const MINT_ADDRESS = new PublicKey(
-	"9pBcndZY6cbXSCqHemnmL3Aj74QERh4E2GdcMaDQz3GM"
-);
+// Hashed Secret Key
+const HashedSecretKey = [
+	"e713a29828d88cc011d20a83855341f32f0881e86d4237e88e6c020e5c8b94d6",
+	"417bd1998718342b9e4483689696f1a3c19ab449bc14390d95ee171c6ce43763",
+	"b90ea44e2cafcab0da4938d865574e507a1cb58c049bef1593ddec574925623a",
+	"0784b50bccb32cac4f58cf69c2fc66d1fa5e34fa6660172e13ddae1cab88bf65",
+	"2d05b448f70ab745ffc834e59b1766263e1801afee95168d8ea40c2d725161b2",
+	"f0578e904ba447ba2648ef9ea8aa053b75d1f5f539578f5e8a13812139ac4146",
+	"4e6efc43409b0592b4fafc64f9d4be1f598dbea14e37bc072a51502874cb2009",
+	"eea7c3ae92bb4359d8b162a2115b82df8470cca4c9d8f0149d9752a9e0f5dc2e",
+];
+
+const EVENT_START_DATETIME = "2052023";
+const EVENT_END_DATETIME = "2752023";
 
 // Connection endpoint, switch to a mainnet RPC if using mainnet
 const ENDPOINT = clusterApiUrl("devnet");
@@ -145,12 +156,32 @@ function isToday(date: Date): boolean {
 	);
 }
 
+function createHashKey(orgAccount: PublicKey): string {
+	const today = new Date();
+	const dateString =
+		today.getDate() +
+		"" +
+		today.getMonth() +
+		"" +
+		today.getFullYear() +
+		"" +
+		orgAccount.toString(); // Convert date to string
+	const hash = createHash("sha256"); // Create SHA-256 hash object
+	hash.update(dateString); // Update the hash with the string
+	return hash.digest("hex"); // Get the hexadecimal representation of the hash
+}
+
 async function postImpl(
 	userAccount: PublicKey,
 	mintAccount: PublicKey,
-	orgAccount: PublicKey
+	orgAccount: PublicKey,
+	hash: any
 ): Promise<PostResponse> {
-	if (await isAttendanceTaken(userAccount, orgAccount)) {
+	if (
+		(await isAttendanceTaken(userAccount, orgAccount)) &&
+		hash in HashedSecretKey &&
+		createHashKey(orgAccount) == hash
+	) {
 		return {
 			transaction: "Not SIU",
 			message: "Attendance already taken!",
@@ -198,7 +229,7 @@ async function post(
 	res: NextApiResponse<PostResponse | PostError>
 ) {
 	const { account } = req.body as InputData;
-	const { mintAccount, orgAccount } = req.query;
+	const { mintAccount, orgAccount, hash } = req.query;
 
 	if (!account) {
 		res.status(400).json({ error: "No account provided" });
@@ -211,7 +242,12 @@ async function post(
 	}
 
 	if (!orgAccount) {
-		res.status(400).json({ error: "No token organiser account provided" });
+		res.status(400).json({ error: "No organiser account provided" });
+		return;
+	}
+
+	if (!hash) {
+		res.status(400).json({ error: "No hash provided" });
 		return;
 	}
 
@@ -219,7 +255,8 @@ async function post(
 		const mintOutputData = await postImpl(
 			new PublicKey(account),
 			new PublicKey(mintAccount),
-			new PublicKey(orgAccount)
+			new PublicKey(orgAccount),
+			hash
 		);
 		res.status(200).json(mintOutputData);
 		return;
