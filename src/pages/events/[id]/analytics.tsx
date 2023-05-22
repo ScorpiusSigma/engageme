2a741de73e1549f9df86cc9ed2ec44d17c416410
Randell Crapy <randell.crapy@gmail.com>
2023-05-22 17:32:06 +0800

    Half done table with 2 view modes annd filtering abilities

--- src/pages/events/[id]/analytics.tsx
+++ src/pages/events/[id]/analytics.tsx
@@ -1,14 +1,44 @@
 import ClaimGraph from "@/components/ClaimGraph";
 import DailyAttenGraph from "@/components/DailyAttenGraph";
 import Navbar from "@/components/Navbar/Navbar";
+import { tableCellStyle } from "@/utils";
 import router from "next/router";
 import { useEffect, useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
+import { DateValueType } from "react-tailwindcss-datepicker/dist/types";
 
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { FormControl, IconButton, InputLabel, MenuItem, Select } from "@mui/material";
+
+enum TableMode {
+    daily,
+    dayRange
+}
+
+const FilterMenu = ({mode}: {
+    mode: TableMode
+}) => {
+    let toDisplay = (
+        <></>
+    )
+    switch (mode){
+        case TableMode.daily:
+            break;
+        case TableMode.dayRange:
+            break;
+    }
+    return toDisplay
+}
 
 
 export default function AnalyticsPage() {
     const [attenMetric, setAttenMetrics] = useState<any[] | undefined>(undefined);
     const [dailyTotal, setDailyTotal] = useState<any[] | undefined>(undefined);
+
+    const [curDate, setCurDate] = useState<DateValueType>(null);
+    const [menuActive, setMenuActive] = useState<Boolean>(false);
+    const [filtPart, setFiltPart] = useState<any[]>([]);
+    const [tableMode, setTableMode] = useState<TableMode>(TableMode.daily);
 
     const [pCnt, setPCnt] = useState<number | undefined>();
     const [claimCnt, setClaimCnt] = useState<number | undefined>();
@@ -34,7 +64,9 @@ export default function AnalyticsPage() {
         if (res.status != 200) {
             return
         }
-        setAttenMetrics((await res.json()).data)
+        const data = (await res.json()).data
+        setAttenMetrics(data)
+        console.log(data)
     }
 
     const fetchAttenAgg = async () => {
@@ -42,7 +74,9 @@ export default function AnalyticsPage() {
         if (res.status != 200) {
             return
         }
-        setDailyTotal((await res.json()).data)
+        const data = (await res.json()).data
+        setDailyTotal(data)
+        // console.log(data)
     }
 
     useEffect(() => {
@@ -58,7 +92,10 @@ export default function AnalyticsPage() {
             <div className="relative pt-16 mx-4">
                 <div className="flex justify-between items-center">
                     {dailyTotal != undefined ? (
-                        <DailyAttenGraph width={600} height={400} data={dailyTotal} className="" />
+                        <div>
+                            <h2> Daily Attendance </h2>
+                            <DailyAttenGraph width={600} height={400} data={dailyTotal} className="" />
+                        </div>
                     ) : (
                         <div> No attendance taken yet </div>
                     )}
@@ -70,6 +107,7 @@ export default function AnalyticsPage() {
                                     height: 400
                                 }}
                             >
+                                <h2>Claim percentage!</h2>
                                 <ClaimGraph data={
                                     [
                                         {
@@ -83,10 +121,85 @@ export default function AnalyticsPage() {
                             </div>
                         )
                     }
-                    {/* claaiums <div>{pCnt-claimCnt}</div>
-                    <div>{claimCnt}</div> */}
 
                 </div>
+                <div className=" rounded-lg bg-white mt-4 shadow-md p-4">
+                    <div className="mb-4 flex justify-between  ">
+                        <div className=" text-xl font-semibold">
+                            Participants
+                        </div>
+                        <IconButton color={menuActive ? "primary" : "default"} onClick={() => {
+                            setMenuActive(!menuActive);
+                        }}>
+                            <FilterListIcon />
+                        </IconButton>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        {menuActive && (
+                            <div className=" w-80 h-full">
+                                <div>
+                                    <div>View Modes</div>
+                                    <FormControl fullWidth>
+                                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={age}
+                                        label="Age"
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                    </Select>
+                                    </FormControl>
+                                </div>
+                                {/* Filter params */}
+                                <div>
+                                    {}
+                                </div>
+                            </div>
+                        )}
+                        <table className="w-full">
+                            <thead className="w-full text-left">
+                                <tr>
+                                    <th className={tableCellStyle + " w-1/2"}>
+                                        Account
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {attenMetric && attenMetric.length > 0 ? (
+                                    <>
+                                        {attenMetric.map(
+                                            ({ account, datetime }, idx) => (
+                                                <tr key={idx}>
+                                                    <td className={tableCellStyle}>
+                                                        {account}
+                                                    </td>
+                                                </tr>
+                                            )
+                                        )}
+                                    </>
+                                ) : (
+
+                                    <div
+                                        className={
+                                            tableCellStyle + "border-none"
+                                        }
+                                    >
+                                        There are no attendees yet :(
+                                    </div>
+
+                                )}
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+
             </div>
 
 
